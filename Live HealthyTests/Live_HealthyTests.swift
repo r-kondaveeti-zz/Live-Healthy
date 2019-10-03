9977bee74b2bbb36bda5d2591db18562dbb4394d
Nolan Schoenle <nmihicschoenle@gmail.com>
2019-10-03 14:18:54 -0500

    Updated plist files for watch bundle

---
+++ Live HealthyTests/Live_HealthyTests.swift
@@ -0,0 +1,35 @@
+//
+//  Live_HealthyTests.swift
+//  Live HealthyTests
+//
+//  Created by Nolan Schoenle on 10/3/19.
+//  Copyright © 2019 Yash Tech. All rights reserved.
+//
+
+import XCTest
+@testable import Live_Healthy;
+
+class Live_HealthyTests: XCTestCase {
+   // var sut: ;
+    
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
