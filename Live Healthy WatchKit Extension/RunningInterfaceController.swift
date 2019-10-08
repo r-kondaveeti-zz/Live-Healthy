89ffad95e65ed663000c04c7f6e00e6204b973e4
Radhithya <kondaveetiradithyareddy@sau.edu>
2019-10-08 11:17:43 -0500

    Accelerometer code moved to watch

--- Live Healthy WatchKit Extension/RunningInterfaceController.swift
+++ Live Healthy WatchKit Extension/RunningInterfaceController.swift
@@ -8,13 +8,43 @@
 
 import WatchKit
 import Foundation
+import CoreMotion
+import WatchConnectivity
 
-
-class RunningInterfaceController: WKInterfaceController {
-
+class RunningInterfaceController: WKInterfaceController, WCSessionDelegate {
+    
+    var motionManager: CMMotionManager!
+    var gravityStr: String!
+    var userAccelStr:String!
+    var rotationRateStr:String!
+    var attitudeStr:String!
+    
+    var totalTime = 0.0;
+    
+    //Session between iPhone and Apple Watch
+    let session = WCSession.default;
+    
+    @IBOutlet weak var axisX: WKInterfaceButton!
+    @IBOutlet weak var axisY: WKInterfaceButton!
+    @IBOutlet weak var axisZ: WKInterfaceButton!
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-    }
+        let session = WCSession.default
+        session.delegate = self;
+        session.activate()
+        //MotionManager
+        motionManager = CMMotionManager()
+        motionManager.deviceMotionUpdateInterval = 1.0 / 50
+        motionManager.startDeviceMotionUpdates(to: OperationQueue.current!) { (deviceMotion: CMDeviceMotion?, error: Error?) in
+                if error != nil {
+                   print("Encountered error: \(error!)")
+                }
+                if deviceMotion != nil {
+                   self.processDeviceMotion(deviceMotion!)
+                }
+            }
+        }
 
     override func willActivate() {
         super.willActivate()
@@ -28,4 +58,52 @@ class RunningInterfaceController: WKInterfaceController {
         pop()
     }
     
+    func messageToPhone() {
+        if WCSession.default.isReachable {
+            session.sendMessage(["timer": String(format: "%.1f", totalTime)], replyHandler: nil, errorHandler: nil)
+             print("This is sending message to the phone ")
+        }
+    }
+
+    func processDeviceMotion(_ deviceMotion: CMDeviceMotion) {
+        // 1. These strings are to show on the UI. Trying to fit
+        // x,y,z values for the sensors is difficult so we’re
+        // just going with one decimal point precision.
+        gravityStr = String(format: "X: %.1f Y: %.1f Z: %.1f" ,
+                            deviceMotion.gravity.x,
+                            deviceMotion.gravity.y,
+                            deviceMotion.gravity.z)
+        userAccelStr = String(format: "X: %.1f Y: %.1f Z: %.1f" ,
+                           deviceMotion.userAcceleration.x,
+                           deviceMotion.userAcceleration.y,
+                           deviceMotion.userAcceleration.z)
+        rotationRateStr = String(format: "X: %.1f Y: %.1f Z: %.1f" ,
+                              deviceMotion.rotationRate.x,
+                              deviceMotion.rotationRate.y,
+                              deviceMotion.rotationRate.z)
+        attitudeStr = String(format: "r: %.1f p: %.1f y: %.1f" ,
+                                 deviceMotion.attitude.roll,
+                                 deviceMotion.attitude.pitch,
+                                 deviceMotion.attitude.yaw)
+        
+        axisX.setTitle(String(deviceMotion.userAcceleration.x))
+        totalTime = deviceMotion.userAcceleration.x;
+        axisY.setTitle(String(deviceMotion.userAcceleration.y))
+        axisZ.setTitle(String(deviceMotion.userAcceleration.z))
+        
+        self.messageToPhone();
+    }
+    
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        switch activationState {
+        case .activated:
+            print("Watch WC Session activated")
+        case .notActivated:
+            print("Watch WC Session not activated")
+        case .inactive:
+            print("Watch WC Session inactivate")
+        @unknown default:
+            print("This is the default switch case on the Apple Watch")
+        }
+    }
 }
--- Live Healthy/ViewController.swift
+++ Live Healthy/ViewController.swift
@@ -12,11 +12,11 @@ import CoreMotion
 @available(iOS 13.0, *)
 class ViewController: UIViewController, WCSessionDelegate {
     
-    var motionManager: CMMotionManager!
-    var gravityStr: String!
-    var userAccelStr:String!
-    var rotationRateStr:String!
-    var attitudeStr:String!
+//    var motionManager: CMMotionManager!
+//    var gravityStr: String!
+//    var userAccelStr:String!
+//    var rotationRateStr:String!
+//    var attitudeStr:String!
     
    
     @IBOutlet weak var durationLabel: UILabel!
@@ -30,17 +30,17 @@ class ViewController: UIViewController, WCSessionDelegate {
             session.delegate = self
             session.activate()
             
-            //MotionManager
-            motionManager = CMMotionManager()
-            motionManager.deviceMotionUpdateInterval = 1.0 / 50
-            motionManager.startDeviceMotionUpdates(to: OperationQueue.current!) { (deviceMotion: CMDeviceMotion?, error: Error?) in
-                    if error != nil {
-                       print("Encountered error: \(error!)")
-                    }
-                    if deviceMotion != nil {
-                       self.processDeviceMotion(deviceMotion!)
-                    }
-                }
+//            //MotionManager
+//            motionManager = CMMotionManager()
+//            motionManager.deviceMotionUpdateInterval = 1.0 / 50
+//            motionManager.startDeviceMotionUpdates(to: OperationQueue.current!) { (deviceMotion: CMDeviceMotion?, error: Error?) in
+//                    if error != nil {
+//                       print("Encountered error: \(error!)")
+//                    }
+//                    if deviceMotion != nil {
+//                       self.processDeviceMotion(deviceMotion!)
+//                    }
+//                }
             }
         
         }
@@ -74,30 +74,30 @@ class ViewController: UIViewController, WCSessionDelegate {
         }
     }
     
-    func processDeviceMotion(_ deviceMotion: CMDeviceMotion) {
-        // 1. These strings are to show on the UI. Trying to fit
-        // x,y,z values for the sensors is difficult so we’re
-        // just going with one decimal point precision.
-        gravityStr = String(format: "X: %.1f Y: %.1f Z: %.1f" ,
-                            deviceMotion.gravity.x,
-                            deviceMotion.gravity.y,
-                            deviceMotion.gravity.z)
-        userAccelStr = String(format: "X: %.1f Y: %.1f Z: %.1f" ,
-                           deviceMotion.userAcceleration.x,
-                           deviceMotion.userAcceleration.y,
-                           deviceMotion.userAcceleration.z)
-        rotationRateStr = String(format: "X: %.1f Y: %.1f Z: %.1f" ,
-                              deviceMotion.rotationRate.x,
-                              deviceMotion.rotationRate.y,
-                              deviceMotion.rotationRate.z)
-        attitudeStr = String(format: "r: %.1f p: %.1f y: %.1f" ,
-                                 deviceMotion.attitude.roll,
-                                 deviceMotion.attitude.pitch,
-                                 deviceMotion.attitude.yaw)
-        
-        durationLabel.text = String(deviceMotion.userAcceleration.x)
-
-    }
+//    func processDeviceMotion(_ deviceMotion: CMDeviceMotion) {
+//        // 1. These strings are to show on the UI. Trying to fit
+//        // x,y,z values for the sensors is difficult so we’re
+//        // just going with one decimal point precision.
+//        gravityStr = String(format: "X: %.1f Y: %.1f Z: %.1f" ,
+//                            deviceMotion.gravity.x,
+//                            deviceMotion.gravity.y,
+//                            deviceMotion.gravity.z)
+//        userAccelStr = String(format: "X: %.1f Y: %.1f Z: %.1f" ,
+//                           deviceMotion.userAcceleration.x,
+//                           deviceMotion.userAcceleration.y,
+//                           deviceMotion.userAcceleration.z)
+//        rotationRateStr = String(format: "X: %.1f Y: %.1f Z: %.1f" ,
+//                              deviceMotion.rotationRate.x,
+//                              deviceMotion.rotationRate.y,
+//                              deviceMotion.rotationRate.z)
+//        attitudeStr = String(format: "r: %.1f p: %.1f y: %.1f" ,
+//                                 deviceMotion.attitude.roll,
+//                                 deviceMotion.attitude.pitch,
+//                                 deviceMotion.attitude.yaw)
+//
+//        durationLabel.text = String(deviceMotion.userAcceleration.x)
+//
+//    }
 }
 
 
