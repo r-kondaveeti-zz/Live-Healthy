945ca347a9d916b550686dfce9a512746b09ff3b
Radhithya <kondaveetiradithyareddy@sau.edu>
2019-10-09 15:03:56 -0500

    Updated code with steps functionality and app icons.

--- Live Healthy/ViewController.swift
+++ Live Healthy/ViewController.swift
@@ -8,17 +8,23 @@
 import UIKit
 import WatchConnectivity
 import CoreMotion
+import HealthKit
 
 @available(iOS 13.0, *)
 class ViewController: UIViewController, WCSessionDelegate {
     
-//    var motionManager: CMMotionManager!
-//    var gravityStr: String!
-//    var userAccelStr:String!
-//    var rotationRateStr:String!
-//    var attitudeStr:String!
+    let healthStore = HKHealthStore();
+    
+    var motionManager: CMMotionManager!
+    var gravityStr: String!
+    var userAccelStr:String!
+    var rotationRateStr:String!
+    var attitudeStr:String!
     
    
+    @IBOutlet weak var getStepsButton: UIButton!
+    @IBOutlet weak var enableHealthAccessButton: UIButton!
+    @IBOutlet weak var stepLabel: UILabel!
     @IBOutlet weak var durationLabel: UILabel!
     
     let session = WCSession.default
@@ -30,17 +36,21 @@ class ViewController: UIViewController, WCSessionDelegate {
             session.delegate = self
             session.activate()
             
-//            //MotionManager
-//            motionManager = CMMotionManager()
-//            motionManager.deviceMotionUpdateInterval = 1.0 / 50
-//            motionManager.startDeviceMotionUpdates(to: OperationQueue.current!) { (deviceMotion: CMDeviceMotion?, error: Error?) in
-//                    if error != nil {
-//                       print("Encountered error: \(error!)")
-//                    }
-//                    if deviceMotion != nil {
-//                       self.processDeviceMotion(deviceMotion!)
-//                    }
-//                }
+            //Rounded
+            self.getStepsButton.layer.cornerRadius = 10;
+            self.enableHealthAccessButton.layer.cornerRadius = 10;
+            
+            //MotionManager
+            motionManager = CMMotionManager()
+            motionManager.deviceMotionUpdateInterval = 1.0 / 50
+            motionManager.startDeviceMotionUpdates(to: OperationQueue.current!) { (deviceMotion: CMDeviceMotion?, error: Error?) in
+                    if error != nil {
+                       print("Encountered error: \(error!)")
+                    }
+                    if deviceMotion != nil {
+                       self.processDeviceMotion(deviceMotion!)
+                    }
+                }
             }
         
         }
@@ -74,30 +84,77 @@ class ViewController: UIViewController, WCSessionDelegate {
         }
     }
     
-//    func processDeviceMotion(_ deviceMotion: CMDeviceMotion) {
-//        // 1. These strings are to show on the UI. Trying to fit
-//        // x,y,z values for the sensors is difficult so we’re
-//        // just going with one decimal point precision.
-//        gravityStr = String(format: "X: %.1f Y: %.1f Z: %.1f" ,
-//                            deviceMotion.gravity.x,
-//                            deviceMotion.gravity.y,
-//                            deviceMotion.gravity.z)
-//        userAccelStr = String(format: "X: %.1f Y: %.1f Z: %.1f" ,
-//                           deviceMotion.userAcceleration.x,
-//                           deviceMotion.userAcceleration.y,
-//                           deviceMotion.userAcceleration.z)
-//        rotationRateStr = String(format: "X: %.1f Y: %.1f Z: %.1f" ,
-//                              deviceMotion.rotationRate.x,
-//                              deviceMotion.rotationRate.y,
-//                              deviceMotion.rotationRate.z)
-//        attitudeStr = String(format: "r: %.1f p: %.1f y: %.1f" ,
-//                                 deviceMotion.attitude.roll,
-//                                 deviceMotion.attitude.pitch,
-//                                 deviceMotion.attitude.yaw)
-//
-//        durationLabel.text = String(deviceMotion.userAcceleration.x)
-//
-//    }
+    func processDeviceMotion(_ deviceMotion: CMDeviceMotion) {
+        // 1. These strings are to show on the UI. Trying to fit
+        // x,y,z values for the sensors is difficult so we’re
+        // just going with one decimal point precision.
+        gravityStr = String(format: "X: %.1f Y: %.1f Z: %.1f" ,
+                            deviceMotion.gravity.x,
+                            deviceMotion.gravity.y,
+                            deviceMotion.gravity.z)
+        userAccelStr = String(format: "X: %.1f Y: %.1f Z: %.1f" ,
+                           deviceMotion.userAcceleration.x,
+                           deviceMotion.userAcceleration.y,
+                           deviceMotion.userAcceleration.z)
+        rotationRateStr = String(format: "X: %.1f Y: %.1f Z: %.1f" ,
+                              deviceMotion.rotationRate.x,
+                              deviceMotion.rotationRate.y,
+                              deviceMotion.rotationRate.z)
+        attitudeStr = String(format: "r: %.1f p: %.1f y: %.1f" ,
+                                 deviceMotion.attitude.roll,
+                                 deviceMotion.attitude.pitch,
+                                 deviceMotion.attitude.yaw)
+        durationLabel.text = String(gravityStr)
+    }
+    
+    @IBAction func authoriseHealthKitAccess(_ sender: Any) {
+           let healthKitTypes: Set = [
+               // access step count
+               HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.stepCount)!,
+               HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate)!
+           ]
+           healthStore.requestAuthorization(toShare: healthKitTypes, read: healthKitTypes) { (bool, error) in
+               if let e = error {
+                   print("oops something went wrong during authorisation \(e.localizedDescription)")
+               } else {
+                   print("User has completed the authorization flow")
+               }
+           }
+       }
+    
+    func getTodaysSteps(completion: @escaping (Double) -> Void) {
+           
+           let stepsQuantityType = HKQuantityType.quantityType(forIdentifier: .stepCount)!
+           
+           let now = Date()
+           let startOfDay = Calendar.current.startOfDay(for: now)
+           let predicate = HKQuery.predicateForSamples(withStart: startOfDay, end: now, options: .strictStartDate)
+           
+           let stepsQuery = HKStatisticsQuery(quantityType: stepsQuantityType, quantitySamplePredicate: predicate, options: .cumulativeSum) { (_, result, error) in
+               var resultCount = 0.0
+               guard let result = result else {
+                   print("Failed to fetch steps rate")
+                   completion(resultCount)
+                   return
+               }
+               if let sum = result.sumQuantity() {
+                   resultCount = sum.doubleValue(for: HKUnit.count())
+               }
+               DispatchQueue.main.async {
+                   completion(resultCount)
+               }
+           }
+           healthStore.execute(stepsQuery)
+       }
+       
+       @IBAction func getTotalSteps(_ sender: Any) {
+           getTodaysSteps { (result) in
+               print("\(result)")
+               DispatchQueue.main.async {
+                   self.stepLabel.text = "Steps: \(result)"
+               }
+           }
+       }
 }
 
 
